3c9bf721d278818cd3d9e0ec5112ccf146498583
esocea <esocea@suvoda.com>
2018-07-01 19:25:23 +0300

    - profil

--- LicentaApp/Controllers/ProfilController.cs
+++ LicentaApp/Controllers/ProfilController.cs
@@ -4,15 +4,16 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LicentaApp.Controllers.Base;
 using LicentaApp.Domain.Auth;
+using LicentaApp.Domain.ValueObjects;
+using LicentaApp.ViewModels.Utilizatori;
 
 namespace LicentaApp.Controllers
 {
     [Authorize(Roles = AuthConstants.Permisii.AdminUtilizator)]
-    public class ProfilController : Controller
+    public class ProfilController : BaseAppController
     {
-        private LicentaDbContext db = new LicentaDbContext();
-
         // GET: Profil
         public ActionResult Index()
         {
@@ -23,6 +24,55 @@ namespace LicentaApp.Controllers
         {
             var utilizatori = db.Utilizatori.Include(u => u.Magazine).Include(u => u.Roluri);
             return PartialView("Utilizatori/Index", utilizatori.ToList());
+        }
+
+        public ActionResult VizualizeazaProfil()
+        {
+            var utilizatorCurent = User as AppPrincipal;
+            return View(this.db.Utilizatori.Include(x => x.Magazine).Include(x=>x.Roluri).FirstOrDefault(x => x.Id == utilizatorCurent.UserId));
+        }
+
+        [HttpGet]
+        public ActionResult ModificaParola()
+        {
+            var utilizatorCurent = User as AppPrincipal;
+            var model = this.db.Utilizatori.Where(x => x.Id == utilizatorCurent.UserId).Select(x => new ModificaParolaViewModel
+            {
+                Username = x.Username
+            }).FirstOrDefault();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult ModificaParola(ModificaParolaViewModel model)
+        {
+
+
+            var dbUtilizator = this.db.Utilizatori.First(x => x.Username == model.Username);
+
+            if (model.ParolaCurenta != dbUtilizator.Parola)
+            {
+                ModelState.AddModelError("", "Parola introdusa nu este corecta");
+            }
+
+            if (model.ParolaNoua != model.ConfirmaParolaNoua)
+            {
+                ModelState.AddModelError("", "Parola noua nu corespunde cu cea confirmata");
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            this.db.Utilizatori.Attach(dbUtilizator);
+            dbUtilizator.Parola = model.ParolaNoua;
+            var result = this.SaveChages();
+
+            if (result != DbSaveResult.Success)
+            {
+                return View(model);
+            }
+            return RedirectToAction("VizualizeazaProfil");
         }
     }
 }
---
+++ LicentaApp/ViewModels/Utilizatori/ModificaParolaViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LicentaApp.ViewModels.Utilizatori
+{
+    public class ModificaParolaViewModel
+    {
+        public string Username { get; set; }
+        public string ParolaCurenta { get; set; }
+        public string ParolaNoua { get; set; }
+        public string ConfirmaParolaNoua { get; set; }
+    }
+}
